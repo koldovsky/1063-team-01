a3dd1e2851f1cc719956bafe3c3c24e906e17040
Alexander0108 <a.dermanskiy@gmail.com>
2023-09-17 18:43:27 +0300

    Changed the last task in the task-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -30,9 +30,6 @@ function litres(time) {
 }
 
 // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
-function lovefunc(flower1, flower2) {
-  if ((flower1 + flower2) % 2 == 0) {
-    return false;
-  }
-  return true;
+function lovefunc(flower1, flower2){
+  return (flower1 + flower2) % 2 !== 0
 }
