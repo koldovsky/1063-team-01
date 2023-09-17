f0fbb1cea663bb0e3ea9415b957d8fc76fdd71cf
Daniil Dolobashko <72915167+DaniilDolobashko@users.noreply.github.com>
2023-09-17 16:50:46 +0300

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,4 +1,38 @@
-// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
-function evenOrOdd(number) {
-  return number % 2 === 0 ? "Even" : "Odd";
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  if (num < 0) {
+    return num
+  }
+  return -num
+}
+
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+function move (position, roll) {
+  return position + roll * 2
+}
+
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+function greet (name, owner) {
+  if (name == owner) {
+    return 'Hello boss'
+  }
+  return 'Hello guest'
+}
+
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+function litres(time) {
+  return Math.floor(time / 2);
+}
+
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function lovefunc(flower1, flower2) {
+  if ((flower1 + flower2) % 2 == 0) {
+    return false;
+  }
+  return true;
 }
