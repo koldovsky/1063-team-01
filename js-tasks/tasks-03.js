eb0477f79464b1b07a5c39cc4b8ce5d3834d1491
Daniil Dolobashko <72915167+DaniilDolobashko@users.noreply.github.com>
2023-09-20 20:27:47 +0300

    added simple tasks

---
+++ js-tasks/tasks-03.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
+
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  const circumference = 2 * Math.PI * circle.radius;
+  return parseFloat(circumference.toFixed(6));
+}
+
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  const lengthIsFive = [];
+  for (let key in obj) {
+    if (key.length == 5) {
+      lengthIsFive.push(key);
+    }
+    if (obj[key].length == 5) {
+      lengthIsFive.push(obj[key]);
+    } 
+  }
+  return lengthIsFive;
+}
+
+// advanced tasks
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
