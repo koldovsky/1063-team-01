f841a0f020fd068549365eee06197c2ddae6d045
A-ProgLang <122487974+A-ProgLang@users.noreply.github.com>
2023-10-04 22:08:52 +0000

    final fix

--- js/gallery-carousel-animated.js
+++ js/gallery-carousel-animated.js
@@ -14,25 +14,20 @@ function getSlidesPerView() {
 }
 
 function setupCarousel() {
-    // Remove clones if they exist
     slides = slides.filter(slide => !slide.classList.contains('clone'));
 
-    // Add clones at start and end for infinite looping
     const clonesStart = slides.slice(-slidesPerView).map(cloneSlide);
     const clonesEnd = slides.slice(0, slidesPerView).map(cloneSlide);
 
-    // Add all slides to the carousel
     carouselInner.innerHTML = '';
     carouselInner.append(...clonesStart, ...slides, ...clonesEnd);
 
-    // Update slides
     slides = Array.from(carouselInner.children);
 
-    // Start automatic sliding
     setInterval(() => {
         currentIndex = (currentIndex + 1) % slides.length;
         updateCarousel();
-    }, 3000); // Прокручувати кожні 3 секунди (змініть за потребою)
+    }, 3000); 
 }
 
 function cloneSlide(slide) {
@@ -45,13 +40,12 @@ function updateCarousel() {
     carouselInner.style.transition = 'transform 0.5s ease-in-out';
     carouselInner.style.transform = `translateX(-${currentIndex * 100 / slidesPerView}%)`;
 
-    // Перевірка, чи досягнутий кінець каруселі, і якщо так, перейти на початок
     if (currentIndex >= slides.length - slidesPerView) {
         setTimeout(() => {
             currentIndex = slidesPerView;
             carouselInner.style.transition = 'none';
             carouselInner.style.transform = `translateX(-${currentIndex * 100 / slidesPerView}%)`;
-        }, 500); // Час затримки, щоб анімація завершилася перед зміною стилів
+        }, 500); 
     }
 }
 
